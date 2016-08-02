5b3fc1fe12463a040a4839a3ebdff415c20ffeba
hbulens <hendrik.bulens@gmail.com>
2016-08-02 22:06:13 +0200

    Copy GoPiGo code sample + add REST support for robot

---
+++ src/hbulens.GoPiGo/hbulens.GoPiGo.IoT.Headless/Api/MotorController.cs
@@ -0,0 +1,36 @@
+﻿using Devkoes.Restup.WebServer.Attributes;
+using Devkoes.Restup.WebServer.Models.Schemas;
+using Devkoes.Restup.WebServer.Rest.Models.Contracts;
+using GoPiGo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hbulens.GoPiGo.IoT.Headless
+{
+    [RestController(InstanceCreationType.Singleton)]
+    public sealed class MotorController
+    {
+        [UriFormat("/Motors/{action}")]
+        public IGetResponse GetWithSimpleParameters(string action)
+        {
+            IGoPiGo goPigo = DeviceFactory.Build.BuildGoPiGo();
+            Task.Run(async () =>
+            {
+                goPigo.RunCommand(Commands.MoveForward);
+                await Task.Delay(TimeSpan.FromSeconds(10));
+            })
+            .ContinueWith((x) =>
+            {
+                goPigo.RunCommand(Commands.Stop);
+            });
+
+            return new GetResponse(GetResponse.ResponseStatus.OK, new DataReceived()
+            {
+                Action = action
+            });
+        }
+    }
+}
---
+++ src/hbulens.GoPiGo/hbulens.GoPiGo.IoT.Headless/StartupTask.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Devkoes.Restup.WebServer.Rest;
+using Devkoes.Restup.WebServer.Http;
+
+namespace hbulens.GoPiGo.IoT.Headless
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class StartupTask : IBackgroundTask
+    {
+        #region Constructor
+
+        #endregion Constructor
+
+        #region Members
+
+        private BackgroundTaskDeferral _deferral;
+
+        #endregion Members
+
+        #region Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="taskInstance"></param>
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // Register API
+            RestRouteHandler restRouteHandler = new RestRouteHandler();
+            restRouteHandler.RegisterController<MotorController>();
+
+            // Start Web API Host
+            HttpServer httpServer = new HttpServer(8800);
+            httpServer.RegisterRoute("api", restRouteHandler);
+            httpServer.StartServerAsync().Wait();
+
+            _deferral = taskInstance.GetDeferral();
+
+        }
+
+        #endregion Methods
+
+    }
+}
